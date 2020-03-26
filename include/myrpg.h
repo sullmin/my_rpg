99ca21c4c413939e6b1b7b395d9dbda7d40a1143
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-03-26 11:23:31 +0100

    - Add stat_t struct

--- include/myrpg.h
+++ include/myrpg.h
@@ -9,6 +9,7 @@
 #define MY_RPG_H
 
 #include <SFML/Graphics.h>
+#include <stdbool.h>
 
 typedef struct {
     unsigned int width;
@@ -21,16 +22,28 @@ typedef struct particule_effect_s
     framebuffer_t *framebuffer;
 } particule_effect_t;
 
+typedef struct stat_s
+{
+    int agility;
+    int strength;
+    int speed;
+    int health;
+    int resistance;
+    int iq;
+} stat_t;
+
 typedef struct item_s
 {
     char *name;
     char *description;
     bool av_particule;
+    bool is_equiped;
     sfVector2i pos;
     sfVector2f size;
     sfSprite *sprite;
     sfTexture *texture;
-    particule_effect_t effect;
+    particule_effect_t particule;
+    stat_t stats,
 } item_t;
 
 #endif
