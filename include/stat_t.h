2a56dbf3f0cfb1963a16de677aa9cc04b2722204
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-03-31 09:21:15 +0200

    - Add stat_t.h

---
+++ include/stat_t.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2019
+** stat_t.h
+** File description:
+** stat_t.h
+*/
+
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+typedef struct stat_s
+{
+    int agility;
+    int strength;
+    int speed;
+    int health;
+    int resistance;
+    int iq;
+} stat_t;
+
+#endif
