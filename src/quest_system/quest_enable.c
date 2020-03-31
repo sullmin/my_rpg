99ad364a606fdf98c6d2760c9328897b9aa4f0ce
RACAUD Simon <simonracaud@gmail.com>
2020-03-31 10:36:59 +0200

    Feat(quest_enable) : activate a quest && Feat(dialogue_enable) : create file

--- src/quest_system/quest_enable.c
+++ src/quest_system/quest_enable.c
@@ -6,3 +6,22 @@
 */
 
 #include "my_rpg.h"
+
+int quest_enable(game_t *game, size_t id)
+{
+    sfTime now;
+
+    if (!game || id >= game->sysquest.size) {
+        return EXIT_ERROR;
+    } else if (game->sysquest.is_enable[id] == true) {
+        return EXIT_FAILURE;
+    }
+    game->sysquest.is_enable[id] = true;
+    game->sysquest.size_enable += 1;
+    now = sfClock_getElapsedTime(game->clock);
+    game->sysquest.time_begin[id] = now.microseconds;
+    if (game->sysquest.pnj_dialogue != NULL) {
+        dialogue_enable(&game->sysquest, id);
+    }
+    return EXIT_SUCCESS;
+}
