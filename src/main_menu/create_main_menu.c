c51a73200307540ffcdf12e26e3e215f4e669e98
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-04-02 15:13:17 +0200

    feat(create_main_menu) set label of menu button

--- src/main_menu/create_main_menu.c
+++ src/main_menu/create_main_menu.c
@@ -9,22 +9,22 @@
 
 static const char *play_path[] = {
     "asset/sprite/buttons/play.png",
-    "asset/sprite/buttons/play.png",
-    "asset/sprite/buttons/play.png",
+    "asset/sprite/buttons/play_h.png",
+    "asset/sprite/buttons/play_c.png",
     NULL,
 };
 
 static const char *quit_path[] = {
     "asset/sprite/buttons/quit.png",
-    "asset/sprite/buttons/quit.png",
-    "asset/sprite/buttons/quit.png",
+    "asset/sprite/buttons/quit_h.png",
+    "asset/sprite/buttons/quit_c.png",
     NULL,
 };
 
 static const char *option_path[] = {
     "asset/sprite/buttons/option.png",
-    "asset/sprite/buttons/option.png",
-    "asset/sprite/buttons/option.png",
+    "asset/sprite/buttons/option_h.png",
+    "asset/sprite/buttons/option_c.png",
     NULL,
 };
 
@@ -36,10 +36,23 @@ static const sfVector2f button_p[3] = {
     {500, 600}
 };
 
-int create_main_menu(game_t *game)
+const char *font = "asset/font/MADETOMMY.ttf";
+
+static int set_main_menu_button(game_t *game)
 {
     int ret = 0;
 
+    ret += set_texture_button(game->ui.main_menu->play, play_path);
+    ret += set_texture_button(game->ui.main_menu->quit, quit_path);
+    ret += set_texture_button(game->ui.main_menu->option, option_path);
+    ret += set_button_label(game->ui.main_menu->play, "play", font, 50);
+    ret += set_button_label(game->ui.main_menu->quit, "quit", font, 50);
+    ret += set_button_label(game->ui.main_menu->option, "option", font, 50);
+    return ret;
+}
+
+int create_main_menu(game_t *game)
+{
     game->ui.main_menu = malloc(sizeof(main_menu_t));
     if (game->ui.main_menu == NULL)
         return EXIT_ERROR;
@@ -49,10 +62,7 @@ int create_main_menu(game_t *game)
     if (game->ui.main_menu->play == NULL || !game->ui.main_menu->quit
         || !game->ui.main_menu->option)
         return EXIT_ERROR;
-    ret += set_texture_button(game->ui.main_menu->play, play_path);
-    ret += set_texture_button(game->ui.main_menu->quit, quit_path);
-    ret += set_texture_button(game->ui.main_menu->option, option_path);
-    if (ret != EXIT_SUCCESS)
+    if (set_main_menu_button(game) != EXIT_SUCCESS)
         return EXIT_ERROR;
     return EXIT_SUCCESS;
 }
