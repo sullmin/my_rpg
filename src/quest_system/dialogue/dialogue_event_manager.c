9475e303c69e8ba34d22cadd6ffb01ed2e48ad62
RACAUD Simon <simonracaud@gmail.com>
2020-04-02 11:51:17 +0200

    Feat(dialogue_event_manager) : read next sentence

--- src/quest_system/dialogue/dialogue_event_manager.c
+++ src/quest_system/dialogue/dialogue_event_manager.c
@@ -7,6 +7,23 @@
 
 #include "my_rpg.h"
 
+bool dialogue_read_next_sentence(sys_quest_t *sysquest)
+{
+    list_str_t *dialogue = NULL;
+
+    if (sysquest->dialogue_id == IDX_DIALOGUE_END)
+        dialogue = sysquest->pnj_dialogue_end;
+    else if (sysquest->dialogue_id == IDX_DIALOGUE_FAIL)
+        dialogue = sysquest->pnj_dialogue_fail;
+    else
+        dialogue = sysquest->pnj_dialogue[sysquest->dialogue_id];
+    sysquest->curr_sentence = my_str_list_read(dialogue);
+    if (sysquest->curr_sentence == NULL) {
+        return false;
+    }
+    return true;
+}
+
 int dialogue_event_manager(game_t *game, sfEvent *event)
 {
     sys_quest_t *sysquest = &game->sysquest;
@@ -16,6 +33,8 @@ int dialogue_event_manager(game_t *game, sfEvent *event)
     } else if (!event || event->type != sfEvtKeyReleased) {
         return EXIT_FAILURE;
     }
-    
-
+    if (dialogue_read_next_sentence(&game->sysquest) == false) {
+        game->sysquest.play_dialogue = false;
+    }
+    return EXIT_SUCCESS;
 }
