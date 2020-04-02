ef8a7f9ad08f8a5e2e9661760d688e8a794c50cd
RACAUD Simon <simonracaud@gmail.com>
2020-04-02 11:30:30 +0200

    Feat(dialogue_load) : load dialogue file

--- src/quest_system/dialogue/dialogue_load.c
+++ src/quest_system/dialogue/dialogue_load.c
@@ -7,7 +7,25 @@
 
 #include "my_rpg.h"
 
+static const char *EXT_FILE_DIALOGUE = ".diag";
+
 int dialogue_load(list_str_t **dialogue, const char *file)
 {
-    
+    char **content = NULL;
+
+    if (!file)
+        return EXIT_ERROR;
+    else if (!my_file_ext_cmp(file, EXT_FILE_DIALOGUE)) {
+        return puterr("dialogue_load : wrong file extension\n", EXIT_ERROR);
+    }
+    content = my_read_file(file);
+    if (!content)
+        return puterr("dialogue_load : fail to read diag file\n", EXIT_ERROR);
+    if (word_array_len(content) == 0)
+        return puterr("dialogue_load : invalid size file\n", EXIT_ERROR);
+    for (size_t i = 0; content[i] != NULL; i++) {
+        if (my_str_list_add(dialogue, content[i]) == EXIT_ERROR)
+            return puterr("dialogue_load : list add error\n", EXIT_ERROR);
+    }
+    return EXIT_SUCCESS;
 }
