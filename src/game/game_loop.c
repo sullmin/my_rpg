b0b9482eabc1a6b0bdcc856418b7da82b87c69a6
RACAUD Simon <simonracaud@gmail.com>
2020-04-02 14:38:33 +0200

    Style(game) : rename debug function call, test quest system and dialogue

--- src/game/game_loop.c
+++ src/game/game_loop.c
@@ -35,7 +35,7 @@ int game_loop(game_t *game)
 {
     sfEvent event;
 
-    quest_enable(game, 0); // DEBUG
+    quest_enable(game, 0); // DEBUG => quest system dialogue
     game->state = MAIN_MENU;
     while (sfRenderWindow_isOpen(game->w.window)) {
         while (sfRenderWindow_pollEvent(game->w.window, &event))
