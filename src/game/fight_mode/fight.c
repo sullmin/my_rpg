e50a2b1e4641147050b1f7889e343b4616cd484b
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-04-14 15:11:34 +0200

    Edit functions order

--- src/game/fight_mode/fight.c
+++ src/game/fight_mode/fight.c
@@ -7,6 +7,20 @@
 
 #include "my_rpg.h"
 
+static int init_variables(sfClock **inter_clock, sfClock **update,
+                        combination_t **events, game_t *game)
+{
+    if (*inter_clock == NULL)
+        *inter_clock = sfClock_create();
+    if (*update == NULL)
+        *update = sfClock_create();
+    if (*events == NULL)
+        *events = create_fight_events(game);
+    if (*events == NULL || *update == NULL || *inter_clock == NULL)
+        return EXIT_ERROR;
+    return EXIT_SUCCESS;
+}
+
 static bool is_finish(game_t *game, combination_t *events)
 {
     for (int i = 0; i < game->wfight.actions; i++)
@@ -26,11 +40,11 @@ static void spawn_events(game_t *game, combination_t events,
 
 static void update_event_size(key_event_t *key)
 {
-    if (key->toggle && !key->finish) {
+    if (key->toggle) {
         sfText_setCharacterSize(key->text, key->size);
         key->size += key->increase ? 1 : -1;
         if (key->rotation > 0)
-            sfText_setRotation(key->text, key->rotation++);
+            sfText_setRotation(key->text, key->rotation);
         if (key->size > KEYS_MAX_SIZE)
             key->increase = false;
         if (key->size < 0)
@@ -49,34 +63,21 @@ static void event_group_run(game_t *game, combination_t events, sfClock *update)
     }
 }
 
-static int init_variables(sfClock **inter_clock, sfClock **update,
-                        combination_t **events, game_t *game)
-{
-    if (*inter_clock == NULL)
-        *inter_clock = sfClock_create();
-    if (*update == NULL)
-        *update = sfClock_create();
-    if (*events == NULL)
-        *events = create_fight_events(game);
-    if (*events == NULL || *update == NULL || *inter_clock == NULL)
-        return EXIT_ERROR;
-    return EXIT_SUCCESS;
-}
-
-static void manage_keys_pressed(combination_t events)
+static void manage_keys_pressed(combination_t *events)
 {
     bool all_pressed = true;
 
-    for (int i = 0; i < events.nbr_comb; i++)
-        if (events.group[i].toggle && !events.group[i].finish
-        && !sfKeyboard_isKeyPressed(events.group[i].key)) {
+    for (int i = 0; i < events->nbr_comb; i++)
+        if (events->group[i].toggle && !events->group[i].finish
+        && !sfKeyboard_isKeyPressed(events->group[i].key)) {
             all_pressed = false;
             break;
         }
     if (all_pressed)
-        for (int i = 0; i < events.nbr_comb; i++) {
-            events.group[i].finish = true;
-            events.group[i].rotation++;
+        for (int i = 0; i < events->nbr_comb; i++) {
+            events->group[i].increase = false;
+            events->group[i].finish = true;
+            events->group[i].rotation += 5;
         }
 }
 
@@ -92,7 +93,7 @@ int fight_loop(game_t *game)
         spawn_events(game, events[i],
                     sfClock_getElapsedTime(inter_clock).microseconds, i);
         event_group_run(game, events[i], update);
-        manage_keys_pressed(events[i]);
+        manage_keys_pressed(&events[i]);
     }
     if (is_finish(game, events))
         destroy_events(&events, &inter_clock, &update, game->wfight.actions);
