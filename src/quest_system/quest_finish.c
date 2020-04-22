bf744876cb56474d913eea172deaeeb594587404
RACAUD Simon <simonracaud@gmail.com>
2020-04-22 14:14:29 +0200

    Feat(Quest system) quest_check_end : check if quest are finished, quest_finish : chech reward type

--- src/quest_system/quest_finish.c
+++ src/quest_system/quest_finish.c
@@ -9,23 +9,31 @@
 
 extern const quest_t QUEST_ARRAY[];
 
-static void quest_give_reward(game_t *game, size_t id)
+static void quest_give_reward(game_t *game, const size_t id)
 {
+    const quest_t CURR_QUEST = QUEST_ARRAY[id];
 
+    if (CURR_QUEST.type_reward != REW_STAT) {
+        // ADD [ CURR_QUEST.reward_ref_item ] in player inventory
+    }
+    if (CURR_QUEST.type_reward != REW_ITEM) {
+        // ADD [ CURR_QUEST.reward_stat ] in player stat
+    }
 }
 
-int quest_finish(sys_quest_t *quests, game_t *game, size_t id)
+int quest_finish(game_t *game, const size_t id)
 {
     sfInt64 now = sfClock_getElapsedTime(game->clock).microseconds;
 
-    if (!quests)
+    if (!game)
         return EXIT_ERROR;
-    quests->size_enable -= 1;
-    quests->is_active[id] = false;
-    if ((now - quests->time_begin[id]) > QUEST_ARRAY[id].max_duration) {
-        dialogue_play(quests, IDX_DIALOGUE_FAIL);
+    my_printf("QUEST %lu : has been complete !\n", id);
+    QUEST.size_enable -= 1;
+    QUEST.is_active[id] = false;
+    if ((now - QUEST.time_begin[id]) > QUEST_ARRAY[id].max_duration) {
+        dialogue_play(&QUEST, IDX_DIALOGUE_FAIL);
     } else {
-        dialogue_play(quests, IDX_DIALOGUE_END);
+        dialogue_play(&QUEST, IDX_DIALOGUE_END);
         quest_give_reward(game, id);
     }
     return EXIT_SUCCESS;
