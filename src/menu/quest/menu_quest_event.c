42da80059d6af6ff71a56cc2499e9988f5d20cba
RACAUD Simon <simonracaud@gmail.com>
2020-04-23 13:24:07 +0200

    Feat(quest menu) handle event

---
+++ src/menu/quest/menu_quest_event.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** Created: 2020-04-22T19:06:24+02:00 | Author: simon
+*/
+
+#include "my_rpg.h"
+
+void menu_quest_event(game_t *game, sfEvent *event)
+{
+    (void)event;
+    event_button(MENU_QUEST.exit, WINDOW);
+    if (MENU_QUEST.exit->toggle == 2) {
+        game->submenu = NO_MENU;
+    }
+}
