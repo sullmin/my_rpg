06c6e08168986dfa1a778ed2552a9079bf38a0e7
RACAUD Simon <simonracaud@gmail.com>
2020-04-23 13:23:29 +0200

    Feat(quest system) add 9 tests quests

--- src/quest_system/array_quest.c
+++ src/quest_system/array_quest.c
@@ -29,5 +29,167 @@ const quest_t QUEST_ARRAY[NB_QUEST] =
         .reward_ref_item = {'x', 'x', 'x', 0},
         .ref_item_end = {'x', 'x', 'x', 0},
         .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
+    },
+    {
+        .title = "TEST",
+        .text = "TEST",
+        .file_pnj_dialogue = "start.diag",
+        .type_reward = REW_STAT,
+        .reward_stat =
+        {
+            .agility = 0,
+            .strength = 0,
+            .speed = 0,
+            .health = 0,
+            .resistance = 0,
+            .iq = 0
+        },
+        .reward_ref_item = {'x', 'x', 'x', 0},
+        .ref_item_end = {'x', 'x', 'x', 0},
+        .max_duration = 0
     }
 };
