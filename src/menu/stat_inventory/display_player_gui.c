972288385c05707ccaab233cd2259d6dc8633b14
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-04-24 15:13:12 +0200

    - Display player stat with item in inventory

--- src/menu/stat_inventory/display_player_gui.c
+++ src/menu/stat_inventory/display_player_gui.c
@@ -9,9 +9,31 @@
 #include "display_stat.h"
 #include "inventory_system.h"
 
+static void player_stat_filler(stat_t *stat, game_t *game)
+{
+    stat->resistance = 1;
+    stat->strength = 1;
+    stat->agility = 1;
+    stat->health = 100;
+    stat->speed = 1;
+    stat->iq = 1;
+    for (size_t i = 0; i < game->inventory.size; i++) {
+        if (!game->inventory.inventory[i])
+            continue;
+        stat->resistance += game->inventory.inventory[i]->stats.resistance;
+        stat->strength += game->inventory.inventory[i]->stats.strength;
+        stat->agility += game->inventory.inventory[i]->stats.agility;
+        stat->health += game->inventory.inventory[i]->stats.health;
+        stat->speed += game->inventory.inventory[i]->stats.speed;
+        stat->iq += game->inventory.inventory[i]->stats.iq;
+    }
+}
+
 void display_player_gui(game_t *game)
 {
-    display_stat(game->w.window, &game->ui.display_stat,
-        &game->item_load->item->stats);
+    stat_t player_stat = {0};
+
+    player_stat_filler(&player_stat, game);
+    display_stat(game->w.window, &game->ui.display_stat, &player_stat);
     display_inventory(&game->inventory, game->w.window);
 }
