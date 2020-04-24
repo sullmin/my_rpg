8184aee6f8259b20c948244767d4fe9e9697ea06
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-04-24 10:29:37 +0200

    - Fix player move

--- src/game/move/input_manage.c
+++ src/game/move/input_manage.c
@@ -9,25 +9,23 @@
 #include "movement.h"
 #include "my_rpg.h"
 
+static void built_it(movement_t *mov, size_t pos)
+{
+    for (size_t i = 0; i < 4; mov->orient[i] = false, i++);
+    mov->orient[pos] = true;
+    mov->in_move = true;
+}
+
 void input_manage(game_t *game, sfEvent *event)
 {
     option_t *option = game->option;
 
-    for (size_t i = 0; i < 4; game->player_move.orient[i] = false, i++);
-    if (event->key.code == UP_KEY) {
-        game->player_move.orient[3] = true;
-        game->player_move.in_move = true;
-    }
-    if (event->key.code == DOWN_KEY) {
-        game->player_move.orient[2] = true;
-        game->player_move.in_move = true;
-    }
-    if (event->key.code == LEFT_KEY) {
-        game->player_move.orient[1] = true;
-        game->player_move.in_move = true;
-    }
-    if (event->key.code == RIGHT_KEY) {
-        game->player_move.orient[0] = true;
-        game->player_move.in_move = true;
-    }
+    if (event->key.code == UP_KEY)
+        built_it(&game->player_move, 3);
+    if (event->key.code == DOWN_KEY)
+        built_it(&game->player_move, 2);
+    if (event->key.code == LEFT_KEY)
+        built_it(&game->player_move, 1);
+    if (event->key.code == RIGHT_KEY)
+        built_it(&game->player_move, 0);
 }
--- src/game/move/movement_player.c
+++ src/game/move/movement_player.c
@@ -9,22 +9,22 @@
 #include "my_rpg.h"
 
 static void player_in_movement(sfRenderWindow *window, movement_t *mov,
-    float time, sfIntRect rec)
+    float time, sfIntRect *rec)
 {
     if (time > TIME_MOVE) {
         sfClock_restart(mov->clock);
         time = as_seconds(mov->clock);
     }
     if (time < TIME_MOVE * 0.3) {
-        rec.left = rec.width * 1;
+        rec->left = rec->width * 1;
     }
     else if (time < TIME_MOVE * 0.6) {
-        rec.left = rec.width * 2;
+        rec->left = rec->width * 2;
     }
     else {
-        rec.left = rec.width * 3;
+        rec->left = rec->width * 3;
     }
-    sfSprite_setTextureRect(mov->sprite, rec);
+    sfSprite_setTextureRect(mov->sprite, *rec);
     sfRenderWindow_drawSprite(window, mov->sprite, NULL);
 }
 
@@ -35,12 +35,12 @@ void display_player(sfRenderWindow *window, movement_t *mov, bool act_move)
     float time = as_seconds(mov->clock);
 
     mov->in_move = false;
-    for (i = 0; i < 4 && !mov->orient[i]; i++);
+    for (i = 0; i < 3 && !mov->orient[i]; i++);
     rec.top = rec.height * i;
     if (!act_move && time > TIME_MOVE) {
         sfSprite_setTextureRect(mov->sprite, rec);
         sfRenderWindow_drawSprite(window, mov->sprite, NULL);
         return;
     }
-    player_in_movement(window, mov, time, rec);
+    player_in_movement(window, mov, time, &rec);
 }
