7dbf9b3d881c10ff68bd19c34ba81980db402e39
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-04-24 17:41:47 +0200

    - Fix inventory display

--- src/inventory_system/inventory_display.c
+++ src/inventory_system/inventory_display.c
@@ -11,11 +11,11 @@ static void display_item(player_inventory_t *inv, sfRenderWindow *window,
     size_t i)
 {
     sfVector2f pos = {0};
-    size_t x = (i > (inv->size - 1) / 2) ? 1 : 0;
-    size_t y = (inv->size - 1) % ((inv->size - 1) / 2);
+    size_t y = (i < 9) ? 0 : 1;
+    size_t x = (y == 0) ? i : i  - 9;
 
-    pos.x = inv->pos.x + x * (INV_X_SIZE / 9);
-    pos.y = inv->pos.y + y * (INV_Y_SIZE / 2);
+    pos.x = inv->pos.x + x * (INV_X_SIZE / 9) + 15;
+    pos.y = inv->pos.y + y * (INV_Y_SIZE / 2) + 20;
     sfSprite_setPosition(inv->inventory[i]->sprite, pos);
     sfRenderWindow_drawSprite(window, inv->inventory[i]->sprite, NULL);
 }
