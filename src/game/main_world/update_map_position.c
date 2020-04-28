db981aadf5642a84294323bf7d19b8123c610eb2
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-04-28 16:52:06 +0200

    - Add pnj sound

--- src/game/main_world/update_map_position.c
+++ src/game/main_world/update_map_position.c
@@ -23,6 +23,7 @@ void update_map_position(game_t *game)
     WMAIN->rect.left -= WMAIN->shift_rect * 18;
     sfSprite_setTextureRect(WMAIN->sprite, WMAIN->rect);
     if (as_seconds(WMAIN->sound_effect[1].clock) > 0.3) {
+        sfSound_setVolume(WMAIN->sound_effect[1].sound, OPTION->volume);
         sfSound_play(WMAIN->sound_effect[1].sound);
         sfClock_restart(WMAIN->sound_effect[1].clock);
     }
