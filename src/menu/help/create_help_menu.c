f5d351e24da8f9071d4d29e113a498498b23090f
RACAUD Simon <simonracaud@gmail.com>
2020-04-29 11:59:22 +0200

    FEAT(Whole program) Replace constant by config file variables

--- src/menu/help/create_help_menu.c
+++ src/menu/help/create_help_menu.c
@@ -15,31 +15,37 @@ static const char *return_path[] = {
 };
 static const sfVector2f button_s = {300, 75};
 static const sfVector2f button_p = {800, 900};
-const char *filepath = "asset/sprite/man.jpg";
-static const char *font = "asset/font/MADETOMMY.ttf";
 
 static int load_help_img(game_t *game)
 {
+    char *path_back = GET_VAR(game, "MHELP_BACK");
+
+    if (!path_back)
+        return EXIT_ERROR;
     MENU_HELP->spr_help = sfSprite_create();
-    MENU_HELP->ter_help = sfTexture_createFromFile(filepath ,NULL);
+    MENU_HELP->ter_help = sfTexture_createFromFile(path_back ,NULL);
     if (MENU_HELP->spr_help == NULL || MENU_HELP->ter_help == NULL)
         return EXIT_ERROR;
     sfSprite_setTexture(MENU_HELP->spr_help, MENU_HELP->ter_help, sfTrue);
+    free(path_back);
     return EXIT_SUCCESS;
 }
 
 int create_help_menu(game_t *game)
 {
+    char *font_path = GET_VAR(game, "BASE_FONT");
+
     MENU_HELP = malloc(sizeof(help_menu_t));
-    if (MENU_HELP == NULL)
+    if (MENU_HELP == NULL || !font_path)
         return EXIT_ERROR;
     MENU_HELP->main_menu = create_button(button_p, button_s);
     if (MENU_HELP->main_menu == NULL)
         return EXIT_ERROR;
     if (set_button_texture(MENU_HELP->main_menu, return_path) == EXIT_ERROR)
         return EXIT_ERROR;
-    set_button_label(MENU_HELP->main_menu, "return", font, 50);
+    set_button_label(MENU_HELP->main_menu, "return", font_path, 50);
     if (load_help_img(game) == EXIT_ERROR)
         return EXIT_ERROR;
+    free(font_path);
     return EXIT_SUCCESS;
 }
--- src/menu/main/create_main_menu.c
+++ src/menu/main/create_main_menu.c
@@ -44,14 +44,13 @@ static const sfVector2f button_p[4] = {
     {500, 700}
 };
 
-const char *font = "asset/font/MADETOMMY.ttf";
-
-const char *PATH_BACK = "asset/sprite/back_menu.png";
-
 static int set_main_menu_button(game_t *game)
 {
+    char *font = GET_VAR(game, "BASE_FONT");
     int ret = 0;
 
+    if (!font)
+        return EXIT_ERROR;
     ret += set_button_texture(MMENU->play, play_path);
     ret += set_button_texture(MMENU->quit, quit_path);
     ret += set_button_texture(MMENU->option, option_path);
@@ -60,17 +59,23 @@ static int set_main_menu_button(game_t *game)
     ret += set_button_label(MMENU->quit, "quit", font, 50);
     ret += set_button_label(MMENU->option, "option", font, 50);
     ret += set_button_label(MMENU->help, "help", font, 50);
+    free(font);
     return ret;
 }
 
 static int load_texture(game_t *game)
 {
-    MMENU->txr_background = sfTexture_createFromFile(PATH_BACK, NULL);
+    char *path_back = GET_VAR(game, "MMAIN_BACK");
+
+    if (!path_back)
+        return EXIT_ERROR;
+    MMENU->txr_background = sfTexture_createFromFile(path_back, NULL);
     MMENU->spr_background = sfSprite_create();
     if (MMENU->txr_background == NULL || !MMENU->spr_background) {
         return puterr("load main menu texture : fail\n", EXIT_ERROR);
     }
     sfSprite_setTexture(MMENU->spr_background, MMENU->txr_background, true);
+    free(path_back);
     return EXIT_SUCCESS;
 }
 
