feaceb431dc8abee9ed8a2ef20738a28acfd584c
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-04-30 15:52:41 +0200

    Fix put pixels limits

--- lib/graph/src/my_put_pixel.c
+++ lib/graph/src/my_put_pixel.c
@@ -12,7 +12,7 @@
 void my_put_pixel(framebuffer_t *framebuffer, unsigned int x, unsigned int y,
                     sfColor color)
 {
-    if (x <= framebuffer->width && y <= framebuffer->height) {
+    if (x < framebuffer->width && y < framebuffer->height) {
         framebuffer->pixels[framebuffer->width * 4 * y + x * 4 + 0] = color.r;
         framebuffer->pixels[framebuffer->width * 4 * y + x * 4 + 1] = color.g;
         framebuffer->pixels[framebuffer->width * 4 * y + x * 4 + 2] = color.b;
