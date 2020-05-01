c890bcfefcd2aa22bd4945da6c72b264c8292423
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-05-01 22:54:45 +0200

    - Add set color key

--- src/inventory_system/chaine_list.c
+++ src/inventory_system/chaine_list.c
@@ -52,6 +52,19 @@ void distroy_item_list(list_t *list)
     }
 }
 
+static void set_color_key(list_t *end)
+{
+    while (end) {
+        if (my_strcmp(end->item->id, "213") == 0)
+            sfSprite_setColor(end->item->sprite, sfGreen);
+        if (my_strcmp(end->item->id, "313") == 0)
+            sfSprite_setColor(end->item->sprite, sfRed);
+        if (my_strcmp(end->item->id, "413") == 0)
+            sfSprite_setColor(end->item->sprite, sfWhite);
+        end = end->next;
+    }
+}
+
 static list_t *set_all_item(void)
 {
     char *path[] = {"./asset/config/soap.conf", "./asset/config/knife.conf",
@@ -71,6 +84,7 @@ static list_t *set_all_item(void)
         if (!add_one(&ret, tmp))
             return NULL;
     }
+    set_color_key(ret);
     return ret;
 }
 
