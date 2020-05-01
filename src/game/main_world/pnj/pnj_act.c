82d8fa8534d60c25155dd1d2ab77d70e2b025bc0
Sullivan Geslot <sullivan.geslot@epitech.eu>
2020-05-01 16:13:20 +0200

    - Add new pnj without quest

--- src/game/main_world/pnj/pnj_act.c
+++ src/game/main_world/pnj/pnj_act.c
@@ -8,26 +8,42 @@
 #include "my_rpg.h"
 #include "fight.h"
 
-void act_manage(game_t *game, const size_t id)
+static void quest_manage(game_t *game, const size_t id, const size_t quest_id)
+{
+    if (WMAIN->pnj_man.all_pnj[id].go_act)
+        quest_enable(game, quest_id);
+    WMAIN->pnj_man.all_pnj[id].go_act = false;
+}
+
+static void fight_manage(game_t *game, const size_t id)
 {
     pnj_t *pnj_list = WMAIN->pnj_man.all_pnj;
 
+    if ((pnj_list[id].go_act || pnj_list[id].is_hostile) &&
+        play_fight(game->w, (fight_mode_t) {1, 1000, 0.8, 1}) == 1) {
+        WMAIN->pnj_man.all_pnj[id].move.is_static = false;
+        WMAIN->pnj_man.all_pnj[id].go_act = false;
+        if (QUEST.is_active[2])
+            move_item_in_inventory(game, "114");
+    }
+}
+
+void act_manage(game_t *game, const size_t id)
+{
     switch(id) {
         case 2:
-            if (WMAIN->pnj_man.all_pnj[id].go_act)
-                quest_enable(game, 1);
-            WMAIN->pnj_man.all_pnj[id].go_act = false;
+            quest_manage(game, id, 1);
             break;
         case 5:
-            if (WMAIN->pnj_man.all_pnj[id].go_act)
-                quest_enable(game, 2);
-            WMAIN->pnj_man.all_pnj[id].go_act = false;
+            quest_manage(game, id, 2);
+            break;
+        case 7:
+            quest_manage(game, id, 3);
+            break;
+        case 8:
+            quest_manage(game, id, 4);
             break;
         default:
-            if ((pnj_list[id].go_act || pnj_list[id].is_hostile) &&
-                play_fight(game->w, (fight_mode_t) {1, 1000, 0.8, 1}) == 1)
-                WMAIN->pnj_man.all_pnj[id].go_act = false;
-                if (QUEST.is_active[2])
-                    move_item_in_inventory(game, "114");
+            fight_manage(game, id);
     }
 }
